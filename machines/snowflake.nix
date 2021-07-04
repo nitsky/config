48fa9c5002aaeea5163804b27b6cccfc9d3c74d9
David Yamnitsky <david@yamnitsky.com>
2021-07-03 21:22:20 -0400

    fix interception-tools

--- machines/snowflake.nix
+++ machines/snowflake.nix
@@ -92,7 +92,7 @@ inputs: inputs.nixpkgs.lib.nixosSystem {
         enable = true;
         plugins = [ pkgs.interception-tools-plugins.dual-function-keys ];
         udevmonConfig = ''
-          - JOB: "intercept -g $DEVNODE | dual-function-keys -c /etc/dual-function-keys.yaml | uinput -d $DEVNODE"
+          - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.dual-function-keys}/bin/dual-function-keys -c /etc/dual-function-keys.yaml | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
             DEVICE:
               EVENTS:
                 EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
