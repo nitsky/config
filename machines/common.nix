7a448789fd9e377ecc4ff8c10ae2e1d0035063c4
Isabella Tromba <isabella.tromba@gmail.com>
2021-11-16 11:34:29 -0500

    update colors

--- machines/common.nix
+++ machines/common.nix
@@ -10,7 +10,6 @@ inputs:
     "https://cache.nixos.org/"
     "https://nix-community.cachix.org"
     "https://tangram.cachix.org"
-    "http://babyshark:5000"
   ];
   nix.binaryCachePublicKeys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
