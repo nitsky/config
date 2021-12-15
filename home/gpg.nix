c78e29998b26307ebd281679e64f6a2509374b5e
David Yamnitsky <david@yamnitsky.com>
2021-12-15 13:27:16 -0500

    fix gpg

--- home/gpg.nix
+++ home/gpg.nix
@@ -1,15 +1,20 @@
 user:
 { pkgs, ... }: {
+  home.packages = with pkgs; [
+    pinentry
+  ];
   programs.gpg = {
     enable = true;
   };
   home.file.".gnupg/gpg-agent.conf".text = ''
     enable-ssh-support
+    pinentry-program ${pkgs.pinentry.curses}/bin/pinentry
   '';
   home.file.".gnupg/sshcontrol".text = ''
-   ${user.sshKeyKeygrip}
+    ${user.sshKeyKeygrip}
   '';
   programs.zsh.initExtra = ''
+    export GPG_TTY=$(tty)
     export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
     gpgconf --launch gpg-agent
   '';
