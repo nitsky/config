b12f1218e208f5a4d7bff89893b0445d95b74633
David Yamnitsky <david@yamnitsky.com>
2022-07-06 13:32:45 -0400

    update

--- flake.nix
+++ flake.nix
@@ -1,61 +1,47 @@
 {
   inputs = {
-    fenix = {
-      url = "github:nix-community/fenix";
-    };
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-    };
+    fenix = { url = "github:nix-community/fenix"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-22.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/release-22.05";
-    };
+    nixpkgs = { url = "github:nixos/nixpkgs/release-22.05"; };
   };
-  outputs = inputs: {
-    nixosConfigurations = {
-      babybeluga = import ./machines/babybeluga.nix inputs;
-      babyshark = import ./machines/babyshark.nix inputs;
-    };
-    homeConfigurations = {
-      nitsky = inputs.home-manager.lib.homeManagerConfiguration {
-        system = "aarch64-darwin";
-        username = "nitsky";
-        homeDirectory = "/Users/nitsky";
-        extraSpecialArgs = {
-          inherit inputs;
-          system = "aarch64-darwin";
-          hostname = "bigbird";
+  outputs = inputs:
+    let
+      system = "aarch64-darwin";
+      pkgs = import inputs.nixpkgs { inherit system; };
+    in {
+      nixosConfigurations = {
+        babybeluga = import ./machines/babybeluga.nix inputs;
+        babyshark = import ./machines/babyshark.nix inputs;
+      };
+      homeConfigurations = {
+        nitsky = inputs.home-manager.lib.homeManagerConfiguration {
+          inherit system;
+          username = "nitsky";
+          homeDirectory = "/Users/nitsky";
+          extraSpecialArgs = {
+            inherit inputs system;
+            hostname = "piper";
+          };
+          configuration.imports = [ ./users/nitsky.nix ];
         };
-        configuration.imports = [ ./users/nitsky.nix ];
-      };
-      isabella = inputs.home-manager.lib.homeManagerConfiguration {
-        system = "aarch64-darwin";
-        username = "isabella";
-        homeDirectory = "/Users/isabella";
-        extraSpecialArgs = {
-          inherit inputs;
-          system = "aarch64-darwin";
-          hostname = "mba";
+        isabella = inputs.home-manager.lib.homeManagerConfiguration {
+          inherit system;
+          username = "isabella";
+          homeDirectory = "/Users/isabella";
+          extraSpecialArgs = {
+            inherit inputs system;
+            hostname = "clicky";
+          };
+          configuration.imports = [ ./users/isabella.nix ];
         };
-        configuration.imports = [ ./users/isabella.nix ];
+      };
+    } // {
+      devShell.${system} = pkgs.mkShell {
+        packages = with pkgs; [ inputs.home-manager.defaultPackage.${system} ];
       };
     };
-  }
-  //
-  inputs.flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = import inputs.nixpkgs {
-      inherit system;
-    };
-    in
-    {
-      devShell = pkgs.mkShell {
-        packages = with pkgs; [
-          inputs.home-manager.defaultPackage.${system}
-        ];
-      };
-    }
-  );
 }
